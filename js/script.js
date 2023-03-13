84fb8dacec48c74f8089adafff726b6adb9719e2
faried-elrewany <fariedezz4@gmail.com>
2023-03-13 03:20:54 +0200

    update navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-//________________________________________________________________________
-// i learned this in (JONAS html and css udemy course) _thanks alot ^_^
-//https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/?src=sac&kw=html+and+css
-//_______________________________________________________________________
+console.log("Hello world!");
 
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
@@ -19,44 +38,60 @@ allLinks.forEach(function (link) {
         behavior: "smooth",
       });
 
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
+    // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 });
 
+///////////////////////////////////////////////////////////
 // Sticky navigation
 
 const sectionHeroEl = document.querySelector(".section-hero");
-const height = document.querySelector("header").getBoundingClientRect().height;
+
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
     console.log(ent);
-    !ent.isIntersecting
-      ? document.body.classList.add("sticky")
-      : document.body.classList.remove("sticky");
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
+    // In the viewport
+    root: null,
     threshold: 0,
-    rootMargin: `-${height}px`,
+    rootMargin: "-80px",
   }
 );
 obs.observe(sectionHeroEl);
 
-function callback(entries) {
-  const [entry] = entries;
-  console.log(entry.target);
-  entry.target.classList.remove("reveal", entry.isIntersecting);
-  observe.unobserve(entry.target);
-}
-const observe = new IntersectionObserver(callback, { threshold: 0.1 });
+s;
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-document.querySelectorAll("section:not(:first-child)").forEach((sec) => {
-  observe.observe(sec);
-  sec.classList.add("reveal");
-});
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
